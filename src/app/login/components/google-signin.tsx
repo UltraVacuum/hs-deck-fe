040b34341a9c150be6ad683c7d3c9e53b31bb820
vicvinc <sweetchain@qq.com>
2025-11-02 21:43:25 +0800

    ``` feat(auth): 更新 Google 登录回调重定向地址
    将 Google 登录的 `redirectTo` 参数从动态获取的 `origin` 改为使用环境变量 `NEXT_AUTH_REDIRECT_URL`，以支持更灵活的回调地址配置。
    
    同时格式化了组件代码的缩进，提升可读性。
    ```

--- src/app/login/components/google-signin.tsx
+++ src/app/login/components/google-signin.tsx
@@ -6,37 +6,38 @@ import { Button } from "@/components/ui/button"
 
 export default function GoogleSignIn() {
 
-    const signInWithGoogle = async (formData: FormData) => {
-        "use server";
-        const supabase = createClient();
-        const origin = headers().get("origin");
-        const { data, error }: any = await supabase.
-            auth.signInWithOAuth({
-                provider: 'google',
-                options: {
-                    redirectTo: `${origin}/auth/callback`,
-                },
-            })
-        if (data) {
-            // console.log(data.url)
-            return redirect(data.url)
-        }
+  const signInWithGoogle = async (formData: FormData) => {
+    "use server";
+    const supabase = createClient();
+    const origin = headers().get("origin");
+    const redirectUrl = process.env.NEXT_AUTH_REDIRECT_URL
+    const { data, error }: any = await supabase.
+      auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+          redirectTo: redirectUrl,
+        },
+      })
+    if (data) {
+      // console.log(data.url)
+      return redirect(data.url)
     }
+  }
 
-    return (
-        <form action={signInWithGoogle}>
-            <Button type="submit"
-                className="btn-md grow w-full flex 
+  return (
+    <form action={signInWithGoogle}>
+      <Button type="submit"
+        className="btn-md grow w-full flex 
                     items-center justify-center 
                     border border-foreground/20 
                     rounded-md px-4 py-5 text-foreground"
-                variant="outline"
-            >
-                <div className="w-4 h-4 mr-4">
-                    <GoogleIcon />
-                </div>
-                Continue with Google
-            </Button>
-        </form>
-    )
+        variant="outline"
+      >
+        <div className="w-4 h-4 mr-4">
+          <GoogleIcon />
+        </div>
+        Continue with Google
+      </Button>
+    </form>
+  )
 }
